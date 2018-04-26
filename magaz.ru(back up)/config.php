0e4701d20e8c9028a20e4fb1606ce6ce3ef6ac1b
Alpha698 <tsef47@mail.ru>
2018-04-26 21:29:09 +0300

    OpenCart
    elephant

---
+++ magaz.ru(back up)/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://magaz.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://magaz.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Install/Server/OSPanel/domains/magaz.ru/catalog/');
+define('DIR_SYSTEM', 'D:/Install/Server/OSPanel/domains/magaz.ru/system/');
+define('DIR_IMAGE', 'D:/Install/Server/OSPanel/domains/magaz.ru/image/');
+define('DIR_LANGUAGE', 'D:/Install/Server/OSPanel/domains/magaz.ru/catalog/language/');
+define('DIR_TEMPLATE', 'D:/Install/Server/OSPanel/domains/magaz.ru/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/Install/Server/OSPanel/domains/magaz.ru/system/config/');
+define('DIR_CACHE', 'D:/Install/Server/OSPanel/domains/magaz.ru/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Install/Server/OSPanel/domains/magaz.ru/system/storage/download/');
+define('DIR_LOGS', 'D:/Install/Server/OSPanel/domains/magaz.ru/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Install/Server/OSPanel/domains/magaz.ru/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Install/Server/OSPanel/domains/magaz.ru/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'magaz');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
